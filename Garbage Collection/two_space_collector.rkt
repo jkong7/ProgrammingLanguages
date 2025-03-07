ef730ca74568ca209d12800918d328f93ff0c008
Jonathan Kong <jonathankong677@gmail.com>
2025-03-06 19:40:48 -0600

    lots of debugging on GC

--- Garbage Collection/two_space_collector.rkt
+++ Garbage Collection/two_space_collector.rkt
@@ -1,5 +1,21 @@
 #lang plai/gc2/collector
 
+; Jonathan Kong
+; CS 321
+; Winter 2025
+; Homework 6
+
+(define eight-principles
+  (list
+   "Know your rights."
+   "Acknowledge your sources."
+   "Protect your work."
+   "Avoid suspicion."
+   "Do your own work."
+   "Never falsify a record or permit another person to do so."
+   "Never fabricate data, citations, or experimental results."
+   "Always tell the truth when discussing your work with your instructor."))
+
 (print-only-errors)
 
 #|
@@ -9,14 +25,9 @@ pair:    ... | 'cons | <first-ptr> | <rest-ptr> | ...
 closure: ... | 'clos | <code-ptr> | <n-free-vars> | <fv0> | <fv1> | ... | ...  
 |#
 
-;; alloc-pointer: (heap-ref 0)
-;; active-half: (heap-ref 1)
-;; L: (heap-ref 2)
-;; R: (heap-ref 3)
-
 (define (init-allocator)
-  (heap-set! 0 4)
-  (heap-set! 1 'first-half)
+  (heap-set! 0 (+ 4 (/ (- (heap-size) 4) 2))) 
+  (heap-set! 1 (+ 4 (/ (- (heap-size) 4) 2)))
   (heap-set! 2 4)
   (heap-set! 3 4)
   (for ([i (in-range 4 (heap-size))])
@@ -24,128 +35,129 @@ closure: ... | 'clos | <code-ptr> | <n-free-vars> | <fv0> | <fv1> | ... | ...
 
 
 (define (malloc n root1 root2)
-  (if (equal? (heap-ref 1) 'second-half)
-      (cond
-        [(>= (+ n (heap-ref 0)) (heap-size))
-         (collect-garbage root1 root2)
-         (if (>= (+ n (heap-ref 0)) (heap-size))
-             (error 'malloc "out of memory")
-             (begin0 (heap-ref 0)
-               (heap-set! 0 (+ (heap-ref 0) n))))]
-        [else
-         (begin0 (heap-ref 0)
-           (heap-set! 0 (+ (heap-ref 0) n)))])
-      (cond
-        [(>= (+ n (heap-ref 0)) (+ 4 (/ (- (heap-size) 4) 2)))
-         (collect-garbage root1 root2)
-         (if (>= (+ n (heap-ref 0)) 
-                 (+ 4 (/ (- (heap-size) 4) 2)))
-             (error 'malloc "out of memory")
-             (begin0 (heap-ref 0)
-               (heap-set! 0 (+ (heap-ref 0) n))))]
-        [else
-         (begin0 (heap-ref 0)
-           (heap-set! 0 (+ (heap-ref 0) n)))])))
-
+  (define current (heap-ref 0))
+  (define start (heap-ref 1))
+  (define end (+ start (/ (- (heap-size) 4) 2)))
+  (cond
+    [(>= (+ current n) end)
+     (collect-garbage root1 root2)
+     (define newcurrent (heap-ref 0))
+     (define newstart (heap-ref 1))
+     (define newend (+ newstart (/ (- (heap-size) 4) 2)))
+     (cond
+       [(>= (+ newcurrent n)  newend)
+        (error 'malloc "out of memory")]
+       [else
+        (heap-set! 0 (+ newcurrent n))
+        newcurrent])]
+    [else
+     (heap-set! 0 (+ current n))
+     current]))
 
 
 (define (collect-garbage root1 root2)
   (init-queue root1 root2)
-  (process-queue))
-
+  (process-queue)
+  (change-queue-spaces))
 
 (define (process-queue)
-  (define (loop-while)
-    (when (< (heap-ref 2) (heap-ref 3))
-           (case (heap-ref (heap-ref 2))
-             [(flat)
-              (heap-set! 2 (+ (heap-ref 2) 2))]
-             [(cons)
-              (move-or-follow (+ (heap-ref 2) 1))
-              (move-or-follow (+ (heap-ref 2) 2))
-              (heap-set! 2 (+ (heap-ref 2) 3))]
-             [(clos)
-              (for ([i (in-range (heap-ref (+ (heap-ref 2) 2)))])
-                (move-or-follow (+ (heap-ref 2) 3 i)))
-              (heap-set! 2 (+ (heap-ref 2) 3 (heap-ref (+ (heap-ref 2) 2))))]
-             [else 'process-queue "unexpected object ~a" (heap-ref (heap-ref 2))]))
-    (loop-while))
-  (loop-while)
-  (if (equal? (heap-ref 1) 'first-half)
-      (begin
-        (heap-set! 1 'second-half)
-        (heap-set! 0 (+ 4 (/ (- (heap-size) 4) 2))))
-      (begin
-        (heap-set! 1 'first-half)
-        (heap-set! 0 4))))
+  (define (pop-queue addr)
+    (case (heap-ref addr)
+      [(free)
+       (error 'process-queue "invalid address ~a" addr)]
+      [(flat)
+       (heap-set! 2 (+ addr 2))]
+      [(cons)
+       (process-pointer (+ addr 1))
+       (process-pointer (+ addr 2))
+       (heap-set! 2 (+ addr 3))]
+      [(clos)
+       (define n-vars (heap-ref (+ addr 2)))
+       (for ([i (in-range n-vars)])
+         (process-pointer (+ addr 3 i)))
+       (heap-set! 2 (+ addr 3 n-vars))]
+      [else
+       (error 'process-queue "unexpected tag ~a" addr)]))
 
+  (define (bfs-loop)
+    (define L (heap-ref 2))
+    (define R (heap-ref 3))
+    (cond
+      [(>= L R)
+       (void)]
+      [else
+       (pop-queue L)
+       (bfs-loop)]))
+  (bfs-loop))
+
+(define (process-pointer p)
+  (if (equal? (heap-ref (heap-ref p)) 'forwarding)
+      (heap-set! p (heap-ref (+ (heap-ref p) 1)))
+      (let ([oldR (heap-ref 3)])
+        (move-obj (heap-ref p))
+        (heap-set! p oldR))))
 
 (define (init-queue root1 root2)
-  (define heap-start
-    (if (equal? (heap-ref 1) 'second-half)
-        (+ 4 (/ (- (heap-size) 4) 2))
-        4))
-  (heap-set! 2 heap-start)
-  (heap-set! 3 heap-start)
+  (move-or-follow root1)
+  (move-or-follow root2)
+  (move-or-follow (get-root-set)))
 
-  (move-or-skip root1)
-  (move-or-skip root2)
-  (for ([r (in-list (get-root-set))])
-    (move-or-skip r)))
-
-(define (move-or-skip x)
+(define (move-or-follow root)
   (cond
-    [(eq? x #f)
-     (void)]                           
-    [(integer? x)
-     (move-obj x)]                 
-    [(pair? x)
-     (for ([sub (in-list x)])
-       (move-or-skip sub))]
-    [else
-     (void)]))                    
-
-
-(define (move-or-follow pointer)
-  (when (and (integer? pointer)
-             (not (eq? pointer #f)))
-    (case (heap-ref pointer)
-      [(f)
-       (heap-set! pointer (heap-ref (+ 1 pointer)))]
-      [else
-       (move-obj (heap-ref pointer))])))
+    [(false? root) void]
+    [(root? root)
+     (define obj (read-root root))
+     (if (equal? (heap-ref obj) 'forwarding)
+         (set-root! root (heap-ref (+ 1 obj)))
+         (let ([oldR (heap-ref 3)])
+           (move-obj obj)
+           (set-root! root oldR)))]
+    [(list? root)
+     (for ([r (in-list root)])
+       (move-or-follow r))]
+    [else (error 'move-or-follow "unexpected object, got ~a" root)]))
 
 (define (move-obj root)
+  (define R (heap-ref 3))
   (case (heap-ref root)
     [(flat)
-     (heap-set! root 'f)
-     (heap-set! (+ root 1) (heap-ref 3))
-     (heap-set! (heap-ref 3) 'flat)
-     (heap-set! (+ (heap-ref 3) 1) (heap-ref (+ root 1)))
-     (heap-set! 3 (+ (heap-ref 3) 2))] 
+     (heap-set! R 'flat)
+     (heap-set! (+ R 1) (heap-ref (+ root 1)))
+     (heap-set! root 'forwarding)
+     (heap-set! (+ root 1) R)
+     (heap-set! 3 (+ R 2))] 
     [(cons)
-     (heap-set! root 'f)
-     (heap-set! (+ root 1) (+ (heap-ref 3) 1))
-     (heap-set! (+ root 2) (+ (heap-ref 3) 2))
-     (heap-set! (heap-ref 3) 'cons)
-     (heap-set! (+ (heap-ref 3) 1) (heap-ref (+ root 1))) 
-     (heap-set! (+ (heap-ref 3) 2) (heap-ref (+ root 2))) 
-     (heap-set! 3 (+ (heap-ref 3) 3))]  
+     (heap-set! R 'cons)
+     (heap-set! (+ R 1) (heap-ref (+ root 1)))
+     (heap-set! (+ R 2) (heap-ref (+ root 2)))
+     (heap-set! root 'forwarding)
+     (heap-set! (+ root 1) R)
+     (heap-set! 3 (+ R 3))]  
     [(clos)
-     (heap-set! root 'f)
-     (heap-set! (+ root 1) (+ (heap-ref 3) 1))
-     (heap-set! (+ root 2) (+ (heap-ref 3) 2))
-     (for ([i (in-range (heap-ref (+ root 2)))])
-       (heap-set! (+ root 3 i) (+ (heap-ref 3) 3 i))) 
-     (heap-set! (heap-ref 3) 'clos)
-     (heap-set! (+ (heap-ref 3) 1) (heap-ref (+ root 1))) 
-     (heap-set! (+ (heap-ref 3) 2) (heap-ref (+ root 2))) 
-     (for ([i (in-range (heap-ref (+ root 2)))])
-       (heap-set! (+ (heap-ref 3) 3 i) (heap-ref (+ root 3 i))))
-     (heap-set! 3 (+ (heap-ref 3) 3 (heap-ref (+ root 2))))]  
+     (define n-vars (heap-ref (+ root 2)))
+     (heap-set! R 'clos)
+     (heap-set! (+ R 1) (heap-ref (+ root 1)))
+     (heap-set! (+ R 2) (heap-ref (+ root 2)))
+     (for ([i (in-range n-vars)])
+       (heap-set! (+ R 3 i) (heap-ref (+ root 3 i))))
+     (heap-set! root 'forwarding)
+     (heap-set! (+ root 1) R)
+     (heap-set! 3 (+ R 3 n-vars))]  
     [else
-     (error 'move-obj "unexpected object ~a" root)])
-  (set-root! root (heap-ref 3)))
+     (error 'move-obj "unexpected object ~a" root)]))
+
+(define (change-queue-spaces)
+  (define oldR (heap-ref 3))
+  (define oldActiveStart (heap-ref 1))
+  (define newActiveStart
+    (if (= oldActiveStart 4)
+        (+ 4 (/ (- (heap-size) 4) 2))
+        4))
+  (heap-set! 0 oldR)
+  (heap-set! 1 newActiveStart)
+  (heap-set! 2 oldActiveStart)
+  (heap-set! 3 oldActiveStart))
+
 
 ;; ----------------------------------------------------------------------
 
